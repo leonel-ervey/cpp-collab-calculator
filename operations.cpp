22459c363d9d9d4a539a9c56dc51d171c281f324
unknown <a01256421@tec.mx>
2025-11-24 16:33:47 -0800

    Agregar funciones de multiplicación y división.

---
+++ operations.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Hp on 24/11/2025.
+//
+#include "operations.h"
+#include <iostream>
+
+using namespace std;
+double multiply(double a, double b) { return a * b; }
+double divide(double a, double b) {
+    if (b != 0) return a / b;
+    else {
+        cout << "Error: División por cero\n";
+        return 0;
+    }
+}
--- operations.h
+++ operations.h
@@ -2,7 +2,8 @@
 // Created by amavi on 19/11/2025.
 //
 
-#ifndef UNTITLED2_OPERATIONS_H
-#define UNTITLED2_OPERATIONS_H
-
-#endif //UNTITLED2_OPERATIONS_H
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+double multiply (double a, double b);
+double divide (double a, double b);
+#endif //OPERATIONS_H
